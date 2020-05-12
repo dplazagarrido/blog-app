be5a417840e6b1b4e1ad7bef20f5a9a613d22eea
Diego Plaza <d.plazagarrido@gmail.com>
2020-05-12 17:31:08 -0400

    feat: add Context

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const IndexScreen = () => {
+
+  const {state, addBlogPost} = useContext(Context);
+
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPosts) => blogPosts.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
+    </View>
+  );
+};
+
+const stules = StyleSheet.create({});
+
+export default IndexScreen;
