e9aaecbccb440eb599454c2963452066f22acc9b
Diego Plaza <d.plazagarrido@gmail.com>
2020-07-13 09:33:00 -0400

    feat: add navigator routes

--- App.js
+++ App.js
@@ -3,13 +3,19 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
 
 const navigator = createStackNavigator({
-  Index: IndexScreen
+  Index: IndexScreen,
+  Show: ShowScreen,
+  Create: CreateScreen,
+  Edit: EditScreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
-    title: 'Blogs'
+    title: 'Blogs',
   }
 });
 
